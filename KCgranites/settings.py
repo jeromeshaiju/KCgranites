7a11f785f182c49ab54b1bfcddae6c5b4c758000
jerome <jeromeshaijukc2007@gmail.com>
2025-03-02 15:06:06 +0530

    Initial commit or update

--- KCgranites/settings.py
+++ KCgranites/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-xu^!s=y0+y=_gw&be*!&!#-lo9iwf7q!1m@4oj4)fvgicu-kzb'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = os.getenv("DEBUG", "False") == "True"
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
---
+++ server.py
@@ -0,0 +1,1 @@
+from KCgranites.wsgi import application  # or your_project_name.asgi for ASGI apps
